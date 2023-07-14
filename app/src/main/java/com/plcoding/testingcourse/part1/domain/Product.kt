0c4b5a126e173639c175af5cf3d72a72de276d2b
Philipp Lackner <mail@pl-coding.com>
2023-07-14 17:54:28 +0200

    -

--- app/src/main/java/com/plcoding/testingcourse/part1/domain/Product.kt
+++
@@ -1,7 +0,0 @@
-package com.plcoding.testingcourse.part1.domain
-
-data class Product(
-    val id: Int,
-    val name: String,
-    val price: Double,
-)
--- app/src/main/java/com/plcoding/testingcourse/part1/domain/ShoppingCartCache.kt
+++
@@ -1,7 +0,0 @@
-package com.plcoding.testingcourse.part1.domain
-
-interface ShoppingCartCache {
-    fun saveCart(items: List<Product>)
-    fun loadCart(): List<Product>
-    fun clearCart()
-}
