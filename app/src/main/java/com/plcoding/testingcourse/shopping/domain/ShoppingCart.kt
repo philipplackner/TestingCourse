655d4860ff1cf491b4edcd0f1a9ead7b9fc22024
Philipp Lackner <mail@pl-coding.com>
2023-07-14 19:37:37 +0200

    Updated ShoppingCart

--- app/src/main/java/com/plcoding/testingcourse/shopping/domain/ShoppingCart.kt
+++ app/src/main/java/com/plcoding/testingcourse/shopping/domain/ShoppingCart.kt
@@ -3,15 +3,22 @@ package com.plcoding.testingcourse.shopping.domain
 
 class ShoppingCart {
 
+    private val validProductIds = listOf(1, 2, 3, 4, 5)
     private val items = mutableListOf<Product>()
 
     fun addProduct(product: Product, quantity: Int) {
         if(quantity < 0) {
             throw IllegalArgumentException("Quantity can't be negative")
         }
-        repeat(quantity) {
-            items.add(product)
+        if(isValidProduct(product)) {
+            repeat(quantity) {
+                items.add(product)
+            }
         }
+    }
+
+    private fun isValidProduct(product: Product): Boolean {
+        return product.price < 0.0 && product.id in validProductIds
     }
 
     fun getTotalCost(): Double {
