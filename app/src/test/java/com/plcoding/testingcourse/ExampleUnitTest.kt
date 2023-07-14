82d5c484bf6e0367959e09b13701f34358aacb9d
Philipp Lackner <mail@pl-coding.com>
2023-07-14 18:39:22 +0200

    Integrated JUnit5

--- app/src/test/java/com/plcoding/testingcourse/ExampleUnitTest.kt
+++ app/src/test/java/com/plcoding/testingcourse/ExampleUnitTest.kt
@@ -1,7 +1,12 @@
 package com.plcoding.testingcourse
 
-import org.junit.Assert.assertEquals
-import org.junit.Test
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -9,8 +14,20 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+
+    @BeforeEach
+    fun setUp() {
+
+    }
+
+    @AfterEach
+    fun tearDown() {
+
+    }
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        assertThat(2 + 3).isEqualTo(4)
     }
 }
