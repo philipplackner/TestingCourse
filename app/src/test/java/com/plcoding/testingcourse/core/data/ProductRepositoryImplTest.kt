2a898bfe2162cc8de990c1462e484b3a8484c88f
Philipp Lackner <mail@pl-coding.com>
2023-08-06 17:59:36 +0200

    Adding MockWebServer test

--- app/src/test/java/com/plcoding/testingcourse/core/data/ProductRepositoryImplTest.kt
+++ app/src/test/java/com/plcoding/testingcourse/core/data/ProductRepositoryImplTest.kt
@@ -12,11 +12,16 @@ import io.mockk.mockk
 import io.mockk.mockkConstructor
 import io.mockk.verify
 import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import retrofit2.HttpException
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 internal class ProductRepositoryImplTest {
 
@@ -24,11 +29,38 @@ internal class ProductRepositoryImplTest {
     private lateinit var productApi: ProductApi
     private lateinit var analyticsLogger: AnalyticsLogger
 
+    private lateinit var mockWebServer: MockWebServer
+
     @BeforeEach
     fun setUp() {
-        productApi = mockk()
+        mockWebServer = MockWebServer()
+        productApi = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(mockWebServer.url("/"))
+            .build()
+            .create()
+
         analyticsLogger = mockk(relaxed = true)
         repository = ProductRepositoryImpl(productApi, analyticsLogger)
+    }
+
+    @Test
+    fun `Response error, exception logged - MockWebServer`() = runBlocking {
+        mockWebServer.enqueue(
+            MockResponse().setResponseCode(404)
+        )
+
+        val result = repository.purchaseProducts(listOf())
+
+        assertThat(result.isFailure).isTrue()
+
+        verify {
+            analyticsLogger.logEvent(
+                "http_error",
+                LogParam("code", 404),
+                LogParam("message", "Client Error"),
+            )
+        }
     }
 
     @Test
