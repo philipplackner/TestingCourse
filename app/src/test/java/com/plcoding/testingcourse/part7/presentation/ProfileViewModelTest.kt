ef2a19d7647ac1be6ac7909f944632c7c8a7e723
Philipp Lackner <mail@pl-coding.com>
2023-08-07 15:52:17 +0200

    Adding flow test

--- app/src/test/java/com/plcoding/testingcourse/part7/presentation/ProfileViewModelTest.kt
+++ app/src/test/java/com/plcoding/testingcourse/part7/presentation/ProfileViewModelTest.kt
@@ -1,10 +1,12 @@
 package com.plcoding.testingcourse.part7.presentation
 
 import androidx.lifecycle.SavedStateHandle
+import app.cash.turbine.test
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import assertk.assertions.isFalse
 import assertk.assertions.isNull
+import assertk.assertions.isTrue
 import com.plcoding.testingcourse.part7.data.UserRepositoryFake
 import com.plcoding.testingcourse.util.MainCoroutineExtension
 import kotlinx.coroutines.Dispatchers
@@ -61,5 +63,21 @@ class ProfileViewModelTest {
         assertThat(viewModel.state.value.profile).isNull()
         assertThat(viewModel.state.value.errorMessage).isEqualTo("Test exception")
         assertThat(viewModel.state.value.isLoading).isFalse()
+    }
+
+    @Test
+    fun `Test loading state updates`() = runTest {
+        viewModel.state.test {
+            val emission1 = awaitItem()
+            assertThat(emission1.isLoading).isFalse()
+
+            viewModel.loadProfile()
+
+            val emission2 = awaitItem()
+            assertThat(emission2.isLoading).isTrue()
+
+            val emission3 = awaitItem()
+            assertThat(emission3.isLoading).isFalse()
+        }
     }
 }
