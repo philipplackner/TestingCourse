03cd1e06e28e124f960e728c0919bc45fa21bbdc
Philipp Lackner <mail@pl-coding.com>
2023-08-09 16:48:27 +0200

    Adding decline permission test

--- app/src/androidTest/java/com/plcoding/testingcourse/MainActivityTest.kt
+++ app/src/androidTest/java/com/plcoding/testingcourse/MainActivityTest.kt
@@ -1,12 +1,18 @@
 package com.plcoding.testingcourse
 
+import android.Manifest
 import android.content.ComponentName
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.intent.rule.IntentsRule
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.GrantPermissionRule
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiSelector
 import com.plcoding.testingcourse.part12.presentation.ProfileActivity
 import org.junit.Rule
 import org.junit.Test
@@ -15,6 +21,9 @@ class MainActivityTest {
 
     @get:Rule
     val composeRule = createAndroidComposeRule<MainActivity>()
+
+//    @get:Rule
+//    val grantPermissionRule = GrantPermissionRule.grant(Manifest.permission.RECORD_AUDIO)
 
 //    @get:Rule
 //    val intentsRule = IntentsRule()
@@ -32,5 +41,17 @@ class MainActivityTest {
 //        Intents.intended(IntentMatchers.hasAction("MY_ACTION"))
 //    }
 
+    @Test
+    fun testRecordAudioPermissionDenial_showsErrorDialog() {
+        composeRule.onNodeWithText("Record").performClick()
 
+        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        val dontAllowButton = device.findObject(UiSelector().textStartsWith("Don"))
+        dontAllowButton.click()
+
+        composeRule.onNodeWithText("Record").performClick()
+        dontAllowButton.click()
+
+        composeRule.onNodeWithText("Can't record without permission").assertIsDisplayed()
+    }
 }
