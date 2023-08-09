babf83985cbae2bb517164bae6013c6bfbb2bf5e
Philipp Lackner <mail@pl-coding.com>
2023-08-09 15:24:07 +0200

    Initial setup

--- app/src/main/java/com/plcoding/testingcourse/part12/presentation/ProfileActivity.kt
+++ app/src/main/java/com/plcoding/testingcourse/part12/presentation/ProfileActivity.kt
@@ -1,4 +1,4 @@
-package com.plcoding.testingcourse.part13.presentation
+package com.plcoding.testingcourse.part12.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
