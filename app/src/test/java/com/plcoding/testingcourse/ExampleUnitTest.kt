18f18414ff030fe33efeda217bf14c191c191218
K S <lilberah@yahoo.com>
2024-02-21 06:20:18 -0800

    Imported assertK library. Used to update unit tests.

--- app/src/test/java/com/plcoding/testingcourse/ExampleUnitTest.kt
+++ app/src/test/java/com/plcoding/testingcourse/ExampleUnitTest.kt
@@ -1,6 +1,9 @@
 package com.plcoding.testingcourse
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 /**
@@ -9,8 +12,19 @@ import org.junit.jupiter.api.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    @BeforeEach
+    fun setup() {
+
+    }
+
+    @AfterEach
+    fun tearDown() {
+
+    }
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        assertThat(4).isEqualTo(2+2)
     }
 }
